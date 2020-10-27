82dc14b2d670c338fabdb9afb6a04cd5f0daf5b0
Egor <rav266@inbox.ru>
2020-10-28 00:51:19 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -4,7 +4,6 @@ import typing as tp
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
-
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -15,14 +14,34 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    #
+    # CODE
+
+    #plaintext = input()
+    #shift = input()
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for i in plaintext:
+        if i.lower() in alphabet:
+            if i in alphabet:
+                ciphertext = ciphertext + alphabet[(alphabet.find(i) + int(shift)) % len(alphabet)]
+            else:
+                ciphertext = ciphertext + alphabet[(alphabet.find(i.lower()) + int(shift)) % len(alphabet)].upper()
+        else:
+            ciphertext = ciphertext + str(i)
+
+    #print(ciphertext)
+
+    # THE END CODE
+    #
+
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
-
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -33,7 +52,28 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    #
+    # CODE
+
+    #ciphertext = input()
+    #shift = input()
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for i in ciphertext:
+        if i.lower() in alphabet:
+            if i in alphabet:
+                plaintext = plaintext + alphabet[(alphabet.find(i) - int(shift)) % len(alphabet)]
+            else:
+                plaintext = plaintext + alphabet[(alphabet.find(i.lower()) - int(shift)) % len(alphabet)].upper()
+        else:
+            plaintext = plaintext + str(i)
+
+    #print(plaintext)
+
+    # THE END CODE
+    #
+
     return plaintext
 
 
@@ -44,3 +84,9 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     best_shift = 0
     # PUT YOUR CODE HERE
     return best_shift
+
+#
+# TESTING
+
+#encrypt_caesar()
+#decrypt_caesar()
